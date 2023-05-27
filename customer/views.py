bb43913eea5673093b742ceedbe920f9731c6266
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-05-27 18:59:50 +0000

    handling 404

--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,6 @@
 from customer.models import Customer 
 from customer.serializers import CustomerSerializer
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 
 def customers(request):
     # handles business logic
@@ -9,3 +9,13 @@ def customers(request):
     data = Customer.objects.all()
     serializer = CustomerSerializer(data, many = True)
     return JsonResponse({'customers': serializer.data})
+
+def customer(request, id):
+    try:
+      data = Customer.objects.get(pk=id)
+    except Customer.DoesNotExist:
+       raise Http404('Customer does not exist')
+    serializer = CustomerSerializer(data, many = False)
+    return JsonResponse({'customer': serializer.data})
+
+    pass
